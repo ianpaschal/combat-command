b2607fe842b0e6a5b3a6ce8f1cc7c13d10f35087
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-22 08:56:09 +0200

    Remove some unused dependencies (mainly fonts)

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,6 @@ import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from '~/components/AuthProvider';
 import { router } from '~/routes';
 
-import '@fontsource/jost/400.css'; // Normal
-import '@fontsource/jost/500.css'; // Medium
-import '@fontsource/jost/600.css'; // Semi-Bold
-import '@fontsource/jost/700.css'; // Bold
-import '@fontsource/libre-caslon-text/400.css'; // Normal
-import '@fontsource/libre-caslon-text/700.css'; // Bold
 import '~/style/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
