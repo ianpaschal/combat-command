fc7f1625226ba588744ed4b33284336773ddeb8c
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-22 18:23:24 +0200

    Add <FlagCircle/> to <Avatar/>

--- src/components/generic/Avatar/Avatar.tsx
+++ src/components/generic/Avatar/Avatar.tsx
@@ -5,34 +5,45 @@ import {
 } from '@radix-ui/react-avatar';
 import { User } from 'lucide-react';
 
+import { FlagCircle } from '~/components/generic/FlagCircle';
+import { createCn } from '~/utils/componentLib/createCn';
+
 import './Avatar.scss';
 
 export interface AvatarProps {
   avatarUrl?: string;
-  displayName: string;
+  displayName?: string;
   size?: string | number;
+  countryCode?: string;
   onEdit?: () => void;
 }
+
+const cn = createCn('Avatar');
 
 export const Avatar = ({
   avatarUrl,
   displayName,
+  countryCode,
   size = '2.5rem',
   onEdit,
 }: AvatarProps): JSX.Element => (
-  <Root className="AvatarRoot" style={{ width: size, height: size }}>
+  <Root className={cn()} style={{ width: size, height: size }}>
     <Image
-      className="AvatarImage"
+      className={cn('__Image')}
       src={avatarUrl}
       alt={displayName}
     />
-    <Fallback className="AvatarFallback" delayMs={600}>
+    <Fallback className={cn('__Fallback')} delayMs={600}>
       <User />
     </Fallback>
+
     {onEdit && (
       <span onClick={onEdit}>
 
       </span>
+    )}
+    {countryCode && (
+      <FlagCircle className={cn('__Flag')} code={countryCode} size="0.875rem" />
     )}
   </Root>
 );
