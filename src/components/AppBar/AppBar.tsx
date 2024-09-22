f674ee21c354355c6954fbd2fde2bf397c84fcd4
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-22 08:47:47 +0200

    Add <AppLogo/>

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,8 +2,8 @@ import * as Portal from '@radix-ui/react-portal';
 import { useWindowWidth } from '@react-hook/window-size/throttled';
 import { Menu, Plus } from 'lucide-react';
 
-import logo from '~/assets/logo.svg';
 import { AccountMenu } from '~/components/AccountMenu';
+import { AppLogo } from '~/components/AppLogo';
 import { Drawer } from '~/components/generic/Drawer';
 import { IconButton } from '~/components/generic/IconButton';
 import { NavLink, NavLinks } from '~/components/generic/NavLinks';
@@ -33,7 +33,7 @@ export const AppBar = ({
               </Drawer.Trigger>
               <Drawer.Content side="left">
                 <Drawer.Header title="Navigation" hideTitle>
-                  <img src={logo} height={40} width={40} />
+                  <AppLogo className="AppBar__Logo" />
                 </Drawer.Header>
                 <Drawer.Body>
                   <NavLinks orientation="vertical" routes={navItems} />
@@ -42,7 +42,8 @@ export const AppBar = ({
               </Drawer.Content>
             </Drawer.Root>
           )}
-          <img src={logo} height={40} width={40} />
+          {/* <img src={logo} height={40} width={40} /> */}
+          <AppLogo className="AppBar__Logo" />
           {!isMobile && (
             <NavLinks routes={navItems} />
           )}
