28092f11cc548215d0e993bfe73548c9b860adfb
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-22 18:23:09 +0200

    Add <FlagCircle/>

---
+++ src/components/generic/FlagCircle/FlagCircle.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+import '/node_modules/flag-icons/css/flag-icons.min.css';
+import './FlagCircle.scss';
+
+export interface FlagCircleProps {
+  className?: string;
+  code: string;
+  size?: string | number;
+}
+
+export const FlagCircle = ({
+  className,
+  code,
+  size,
+}: FlagCircleProps): JSX.Element => (
+  <div
+    className={clsx('FlagCircle', 'fis', `fi-${code}`, className)}
+    style={size ? { width: size, height: size } : {}}
+  />
+);
---
+++ src/components/generic/FlagCircle/index.ts
@@ -0,0 +1,1 @@
+export { FlagCircle } from './FlagCircle';
