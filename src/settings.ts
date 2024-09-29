d17a15471c90bea65cfb41aa2dda0329f0d7df08
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-29 17:43:09 +0100

    Create settings.ts

---
+++ src/settings.ts
@@ -0,0 +1,3 @@
+export const BREAKPOINT_MOBILE = 352; // 320px + 2 x 16px
+export const BREAKPOINT_TABLET = 688; // 2 x 320px + 3 x 16px
+export const BREAKPOINT_DESKTOP = 1024; // 3 x 320px + 4 x 16px
