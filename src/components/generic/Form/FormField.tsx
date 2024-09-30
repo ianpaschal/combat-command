eb1b24adcf43c9790607c2dd771d70cfe40ec2a4
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-30 20:26:37 +0100

    Allow <FormField/> to work with <Switch/> and <Checkbox/>

--- src/components/generic/Form/FormField.tsx
+++ src/components/generic/Form/FormField.tsx
@@ -7,53 +7,56 @@ import { Controller, useFormContext } from 'react-hook-form';
 import clsx from 'clsx';
 
 import { Label } from '~/components/generic/Label';
-import { createCn } from '~/utils/componentLib/createCn';
+import { bem } from '~/utils/componentLib/bem';
 import { getComponentName } from '~/utils/componentLib/getComponentName';
 
 import './FormField.scss';
 
 export interface FormFieldProps {
-  description?: string;
-  label?: string;
-  name: string;
-  className?: string;
   children: ReactElement;
+  className?: string;
+  description?: string;
   disabled?: boolean;
+  label: string;
+  name?: string;
 }
 
-const cn = createCn('FormField');
+const cn = bem('FormField');
 
 export const FormField = ({
+  children,
   className,
   description,
+  disabled = false,
   label,
   name,
-  disabled = false,
-  children,
   ...props
 }: FormFieldProps): JSX.Element => {
   const { control, formState: { errors } } = useFormContext();
-  const hasError = Boolean(errors[name]);
-  if (isValidElement(children) && ['Switch', 'Checkbox'].includes(getComponentName(children))) {
-    console.log('Use horizontal layout');
-  }
+  const hasError = !!name && !!errors[name];
+  const horizontal = isValidElement(children) && ['Switch', 'Checkbox'].includes(getComponentName(children));
+
   return (
-    <div className={clsx(cn(), className)}>
-      {label && (
-        <Label className={clsx(cn('_Label'), { [cn('_Label-error')]: hasError, [cn('_Label-disabled')]: disabled })}>{label}</Label>
-      )}
-      <Controller
-        control={control}
-        render={({ field }) => (
-          cloneElement(children, { ...field, ...props, hasError, disabled })
-        )}
-        name={name}
-      />
-      {hasError && (
-        <div className={clsx(cn('_Errors'))}>{errors[name]?.message as string}</div>
+    <div className={clsx(cn(), { [`${cn()}-horizontal`]: horizontal }, className)}>
+      <Label className={cn('Label', { hasError, disabled, horizontal })} htmlFor={name}>{label}</Label>
+      {(name && control) ? (
+        <>
+          <Controller
+            control={control}
+            render={({ field }) => (
+              cloneElement(children, { ...field, ...props, className: cn('Input', { hasError, disabled, horizontal }), hasError, disabled })
+            )}
+            name={name}
+          />
+          {hasError && (
+            <div className={clsx(cn('Errors'))}>{errors[name]?.message as string}</div>
+          )}
+        </>
+      ) : (
+        cloneElement(children, { ...props, className: cn('Input', { disabled, horizontal }), disabled })
       )}
       {description && (
-        <div className={clsx(cn('_Description'), { [cn('_Description-error')]: hasError, [cn('_Description-disabled')]: disabled })}>{description}</div>
+        <div className={cn('Description', { hasError, disabled, horizontal })}>{description}</div>
       )}
     </div>
   );
