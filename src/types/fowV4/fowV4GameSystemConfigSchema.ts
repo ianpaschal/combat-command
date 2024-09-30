19f37d7d5ba648c5f28f46290387fd92ad374290
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-09-30 20:27:23 +0100

    Add option to allow historical MWM for FowV4GameSystemConfig

--- src/types/fowV4/fowV4GameSystemConfigSchema.ts
+++ src/types/fowV4/fowV4GameSystemConfigSchema.ts
@@ -8,7 +8,7 @@ export const fowV4GameSystemConfigSchema = z.object({
   era: fowV4EraSchema,
   points: z.number().min(0),
   lessons_from_the_front_version: z.string(),
-  mid_war_monsters: z.union([z.literal('yes'), z.literal('combat'), z.literal('no')]),
+  mid_war_monsters: z.optional(z.union([z.literal('yes'), z.literal('combat'), z.literal('no')])),
   mission_pack_version: z.string(),
   allowed_books: z.array(fowV4BookSchema),
 });
