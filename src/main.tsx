54e01011f3157081d5693a0244be5292d4a07533
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-10-04 17:31:23 +0200

    Change font to "Figtree"

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from '~/components/AuthProvider';
 import { router } from '~/routes';
 
+import '@fontsource/figtree/300-italic.css';
+import '@fontsource/figtree/300.css';
+import '@fontsource/figtree/400-italic.css';
+import '@fontsource/figtree/400.css';
+import '@fontsource/figtree/500-italic.css';
+import '@fontsource/figtree/500.css';
 import '~/style/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
