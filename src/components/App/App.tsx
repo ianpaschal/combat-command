e704b4678299bce59c922485795e8ce590784137
Ian Paschal <ianpaschal@users.noreply.github.com>
2024-10-24 20:14:53 +0200

    Remove MIN_HEIGHT

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,18 @@
 import { Outlet } from 'react-router-dom';
-import { useWindowSize } from '@react-hook/window-size/throttled';
+import { useWindowWidth } from '@react-hook/window-size/throttled';
 
 import { AppBar } from '~/components/AppBar';
 import { useAuth } from '~/components/AuthProvider';
 import { getNavLinksByVisibility } from '~/routes';
-import {
-  MAX_WIDTH,
-  MIN_HEIGHT,
-  MIN_WIDTH,
-} from '~/settings';
+import { MAX_WIDTH, MIN_WIDTH } from '~/settings';
 
 import './App.scss';
 
 export const App = (): JSX.Element => {
   const user = useAuth();
   const links = getNavLinksByVisibility('main');
-  const [width, height] = useWindowSize();
-  if (width < MIN_WIDTH || height < MIN_HEIGHT) {
+  const width = useWindowWidth();
+  if (width < MIN_WIDTH) {
     return (
       <div className="App">
         <p>Please use a larger device to use CombatCommand. If you are currently using your phone in landscape mode, please turn it to portrait mode.</p>
