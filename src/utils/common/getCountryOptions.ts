c31eea3d458222aa3cc2545dd55b1b1b5d11cfd8
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-03-10 14:43:27 +0100

    Small tweaks

--- src/utils/common/getCountryOptions.ts
+++ src/utils/common/getCountryOptions.ts
@@ -19,36 +19,3 @@ export const getCountryOptions = (): InputSelectOption<string>[] => {
     ...britishCountries,
   ].sort((a, b) => a.label.localeCompare(b.label));
 };
-
-const etcCountryCodes = [
-  'nl',
-  'be',
-  'lu',
-  'is',
-  'dk',
-  'de',
-  'se',
-  'no',
-  'fi',
-  'gb-sct',
-  'gb-nir',
-  'ie',
-  'gb-wls',
-  'gb-eng',
-  'ch',
-  'fr',
-  'es',
-  'pt',
-  'it',
-  'at',
-  'nz',
-  'us',
-  'pl',
-];
-
-export const getEtcCountryOptions = (): InputSelectOption<string>[] => (
-  etcCountryCodes.map((code) => ({
-    label: getCountryName(code) || 'Unknown Country',
-    value: code,
-  }))
-);
