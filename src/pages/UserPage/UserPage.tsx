ce7a9d0413dce801f52eadb18ed0789606c806dd
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-03-15 16:19:37 +0100

    Remove unused <UserPage/>

--- src/pages/UserPage/UserPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-import { PageWrapper } from '~/components/PageWrapper';
-
-export const UserPage = (): JSX.Element => (
-  <PageWrapper title="Some User Name">
-    <Outlet />
-  </PageWrapper>
-);
