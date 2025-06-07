7da623b46eae020badf9bdf1f01908645dd3ad81
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-07 21:30:59 +0200

    Use new isTeam tournament prop in <TournamentInfoBlock/>

--- src/components/TournamentInfoBlock/TournamentInfoBlock.utils.ts
+++ src/components/TournamentInfoBlock/TournamentInfoBlock.utils.ts
@@ -4,12 +4,11 @@ export const getLocalizedCompetitorFee = (tournament: Tournament): string => {
   if (!tournament.competitorFee) {
     return 'Free';
   }
-  const isTeam = tournament.competitorSize > 1;
   const { currency, amount } = tournament.competitorFee;
   const locale = navigator.language || 'en-US';
   const localizedFee = new Intl.NumberFormat(locale, {
     style: 'currency',
     currency,
   }).format(amount / 100 );
-  return `${localizedFee} per ${isTeam ? 'team' : 'player'}`;
+  return `${localizedFee} per ${tournament.useTeams ? 'team' : 'player'}`;
 };
