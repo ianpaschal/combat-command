574a76b056b1a28fcec724b9fea0e89cbaf0c005
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-07 20:48:31 +0200

    Add <Table />

---
+++ src/components/generic/Table/Table.types.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+export type RowData = { [key: string]: unknown };
+
+export type ColumnDef<T extends RowData> = {
+  align?: 'left' | 'center' | 'right';
+  className?: string;
+  key: string;
+  label?: string;
+  renderCell?: (row: T) => ReactNode;
+  renderHeader?: () => ReactNode;
+  width?: number;
+};
+
+export type Row<T extends RowData> = [T, number];
