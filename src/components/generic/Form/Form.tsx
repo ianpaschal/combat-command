cd8977f1d5b8f5be62467d9c00f23a8f974fc8d9
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-07 20:58:18 +0200

    Add built-in navigation blocking to <Form/>

--- src/components/generic/Form/Form.tsx
+++ src/components/generic/Form/Form.tsx
@@ -1,10 +1,18 @@
-import { BaseSyntheticEvent, ReactNode } from 'react';
+import {
+  BaseSyntheticEvent,
+  ReactNode,
+  useEffect,
+  useRef,
+} from 'react';
 import {
   FieldValues,
   FormProvider,
   SubmitHandler,
   UseFormReturn,
 } from 'react-hook-form';
+import { useBlocker, useNavigation } from 'react-router-dom';
+
+import { Dialog } from '~/components/generic/Dialog';
 
 export interface FormProps<T extends FieldValues> {
   id?: string;
@@ -21,12 +29,33 @@ export const Form = <T extends FieldValues>({
   className,
   onSubmit,
 }: FormProps<T>) => {
-  const handleSubmit = (e: BaseSyntheticEvent) => {
+  const { isDirty } = form.formState;
+  const blockNavigation = useRef(true);
+  const navigation = useNavigation();
+  const blocker = useBlocker(() => isDirty && blockNavigation.current);
+  const handleSubmit = async (e: BaseSyntheticEvent): Promise<void> => {
     e.stopPropagation();
-    return form.handleSubmit(onSubmit)(e);
+    blockNavigation.current = false;
+    await form.handleSubmit(onSubmit)(e);
   };
+  useEffect(() => {
+    if (navigation.state === 'idle') {
+      blockNavigation.current = true;
+    }
+  }, [navigation.state]);
   return (
     <FormProvider {...form}>
+      <Dialog
+        open={blocker.state === 'blocked'}
+        title="You have unsaved changes"
+        description="Are you sure you want to navigate away? Unsaved changes will be lost."
+        width="small"
+        onCancel={blocker.reset}
+        actions={[
+          { label: 'Cancel', onClick: blocker.reset, variant: 'secondary' },
+          { label: 'Continue', onClick: blocker.proceed, variant: 'primary', intent: 'danger' },
+        ]}
+      />
       <form onSubmit={handleSubmit} className={className} id={id}>
         {children}
       </form>
