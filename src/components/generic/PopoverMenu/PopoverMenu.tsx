63a30d647840112efecbd41c4f78f92fbbaeb201
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-07 20:59:47 +0200

    Improve <PopoverMenu/>

--- src/components/generic/PopoverMenu/PopoverMenu.tsx
+++ src/components/generic/PopoverMenu/PopoverMenu.tsx
@@ -6,8 +6,6 @@ import {
   Trigger,
 } from '@radix-ui/react-popover';
 
-import { Button } from '~/components/generic/Button';
-
 import styles from './PopoverMenu.module.scss';
 
 export interface PopoverMenuProps {
@@ -32,9 +30,9 @@ export const PopoverMenu = ({
     <Content className={styles.PopoverContent} align="end">
       {menuItems.map((menuItem, i) => (
         <Close key={i} asChild>
-          <Button variant="ghost" onClick={menuItem.onClick} className={styles.MenuItemButton}>
+          <div onClick={menuItem.onClick} className={styles.MenuItem}>
             {menuItem.icon}{menuItem.label}
-          </Button>
+          </div>
         </Close>
       ))}
     </Content>
