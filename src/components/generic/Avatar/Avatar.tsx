ee42aa8a60c67e5869f72a75f23a5b1a002441ff
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-07 20:51:42 +0200

    Add muted style to <Avatar/> and other tweaks

--- src/components/generic/Avatar/Avatar.tsx
+++ src/components/generic/Avatar/Avatar.tsx
@@ -1,41 +1,54 @@
+import { ReactElement } from 'react';
 import { Image, Root } from '@radix-ui/react-avatar';
 import clsx from 'clsx';
-import { User } from 'lucide-react';
+import { User, Users } from 'lucide-react';
 
 import { FlagCircle } from '~/components/generic/FlagCircle';
 
 import styles from './Avatar.module.scss';
 
 export interface AvatarProps {
-  url?: string;
   className?: string;
   countryCode?: string;
+  isTeam?: boolean;
+  icon?: ReactElement;
+  muted?: boolean;
   loading?: boolean;
+  url?: string;
   userId?: string;
 }
 
 export const Avatar = ({
-  url,
   className,
   countryCode,
-  loading,
+  isTeam = false,
+  muted = false,
+  loading = false,
+  icon,
+  url,
 }: AvatarProps): JSX.Element => {
-  const getInnerContent = (): JSX.Element | null => {
+  const getInnerContent = (): ReactElement | null => {
     if (loading) {
       return null;
     }
+    if (icon) {
+      return icon;
+    }
     if (url) {
       return <Image className={styles.Image} src={url} />;
+    }
+    if (isTeam) {
+      return <Users />;
     }
     return <User />;
   };
   return (
     <Root className={clsx(styles.Root, className)}>
-      <div className={clsx(styles.Content, { [styles.ContentLoading]: loading })}>
+      <div className={clsx(styles.Content, { [styles.ContentLoading]: loading, [styles['Content-muted']]: muted })}>
         {getInnerContent()}
       </div>
       {countryCode && (
-        <FlagCircle className={styles.Flag} code={countryCode} size="1rem" />
+        <FlagCircle className={styles.Flag} code={countryCode} />
       )}
     </Root>
   );
