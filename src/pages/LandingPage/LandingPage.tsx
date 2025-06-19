6d6de317d65bbbe0b6c5a4c57888c1074833d503
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-06-19 13:40:49 +0200

    feat: #55 Improve home page (#81)

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,49 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { PageWrapper } from '~/components/PageWrapper';
+import { Button } from '~/components/generic/Button';
+import { ScrollArea } from '~/components/generic/ScrollArea';
 import { PreventAuth } from '~/components/PreventAuth';
 import { PATHS } from '~/settings';
 
-export const LandingPage = (): JSX.Element => (
-  <PreventAuth>
-    <PageWrapper>
-      <h2>Welcome to Combat Command</h2>
-      <Link to={PATHS.authSignIn}>Sign In</Link> | <Link to={PATHS.authSignUp}>Sign Up</Link>
-    </PageWrapper>
-  </PreventAuth>
-);
+import styles from './LandingPage.module.scss';
+
+export const LandingPage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const handleSignUp = (): void => {
+    navigate(PATHS.authSignUp);
+  };
+  return (
+    <PreventAuth>
+      <ScrollArea className={styles.LandingPage}>
+        <div className={styles.LandingPage_Content}>
+          <div className={styles.LandingPage_Hero}>
+            <div className={styles.LandingPage_HeroContent}>
+              <h1>Combat Command</h1>
+              <p>Elevate your historical war-gaming experience.</p>
+              <p>Whether you're a casual player or the organizer of a highly competitive tournament, Combat Command is the digital half of your dice box.</p>
+              <Button onClick={handleSignUp} className={styles.LandingPage_HeroCta}>
+                Sign Up
+              </Button>
+            </div>
+          </div>
+          <div className={styles.LandingPage_Normal}>
+            <div className={styles.LandingPage_TripleBlock}>
+              <div className={styles.LandingPage_Block}>
+                <h2>Organize Tournaments</h2>
+                <p>Born out of the European Team Championships for Battlefront’s classic WWII game: Flames of War, CombatCommand offers all the tools organizers need and is battle tested at the highest levels of competition.</p>
+              </div>
+              <div className={styles.LandingPage_Block}>
+                <h2>Check In Matches</h2>
+                <p>Tournaments aren’t everything! Check in your single matches with friends, attach photos, and save notes with the game results.</p>
+              </div>
+              <div className={styles.LandingPage_Block}>
+                <h2>Statistics</h2>
+                <p>What is your most-valuable unit? Do you have an edge playing a certain stance? Which missions do you struggle with the most? The CombatCommand statistics panel gives you insight into all of these topics.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ScrollArea>
+    </PreventAuth>
+  );
+};
