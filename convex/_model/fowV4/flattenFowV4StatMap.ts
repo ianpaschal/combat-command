85245b1d3ae9d13040968d5672a697f9417bb7a6
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-07-01 07:32:48 +0200

    feat: Hide players with 0 matches from rankings

--- convex/_model/fowV4/flattenFowV4StatMap.ts
+++ convex/_model/fowV4/flattenFowV4StatMap.ts
@@ -13,7 +13,7 @@ import {
 
 export const flattenFowV4StatMap = <T extends FowV4StatId>(
   statMap: Record<T, FowV4TournamentExtendedStats>,
-): { id: T; stats: FowV4TournamentFlatExtendedStats }[] => {
+): { id: T; stats: FowV4TournamentFlatExtendedStats, gamesPlayed: number }[] => {
   const statList = Object.entries(statMap) as [T, FowV4TournamentExtendedStats][];
   return statList.map(([key, stats]) => {
     const flatStats = {} as FowV4TournamentFlatExtendedStats;
@@ -27,6 +27,7 @@ export const flattenFowV4StatMap = <T extends FowV4StatId>(
     return {
       id: key,
       stats: flatStats,
+      gamesPlayed: stats.gamesPlayed ?? 0,
     };
   });
 };
