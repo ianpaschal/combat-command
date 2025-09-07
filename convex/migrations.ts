6faa5dcc785c12ae76467db279591496e3c4b7cc
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-09-07 22:22:01 +0200

    #219 Clean up tournament registration migrations

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -6,39 +6,18 @@ import { DataModel, Id } from './_generated/dataModel.js';
 export const migrations = new Migrations<DataModel>(components.migrations);
 export const run = migrations.runner();
 
-export const convertPlayersToRegistrations = migrations.define({
+export const addCaptains = migrations.define({
   table: 'tournamentCompetitors',
   migrateOne: async (ctx, doc) => {
-    if (doc.players) {
-      for (const player of doc.players) {
-        await ctx.db.insert('tournamentRegistrations', {
-          ...player,
-          tournamentCompetitorId: doc._id,
-          tournamentId: doc.tournamentId,
-          listApproved: true,
-          userConfirmed: true,
+    if (!doc.captainUserId) {
+      const captainRegistration = await ctx.db.query('tournamentRegistrations')
+        .withIndex('by_tournament_competitor', (q) => q.eq('tournamentCompetitorId', doc._id))
+        .first();
+      if (captainRegistration) {
+        await ctx.db.patch(doc._id, {
+          captainUserId: captainRegistration.userId,
         });
       }
-      await ctx.db.patch(doc._id, {
-        players: undefined,
-      });
-    }
-  },
-});
-
-export const convertTournamentOrganizers = migrations.define({
-  table: 'tournaments',
-  migrateOne: async (ctx, doc) => {
-    if (doc.organizerUserIds) {
-      for (const userId of doc.organizerUserIds) {
-        await ctx.db.insert('tournamentOrganizers', {
-          userId,
-          tournamentId: doc._id,
-        });
-      }
-      await ctx.db.patch(doc._id, {
-        organizerUserIds: undefined,
-      });
     }
   },
 });
