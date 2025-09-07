0c0f25df778a6fd88e1a3d53f0653c8cf771c3b0
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-09-07 21:50:18 +0200

    #216 Min width prevents users from viewing site

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,13 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import { useWindowWidth } from '@react-hook/window-size/throttled';
-import clsx from 'clsx';
 
 import { AppBar } from '~/components/AppBar';
 import { ErrorBoundary } from '~/components/ErrorBoundary';
-import { MAX_WIDTH, MIN_WIDTH } from '~/settings';
+import { MAX_WIDTH } from '~/settings';
 
 import styles from './App.module.scss';
 
 export const App = (): JSX.Element => {
   const location = useLocation();
-  const width = useWindowWidth();
-  if (width < MIN_WIDTH) {
-    return (
-      <div className={clsx(styles.App, styles['App-minWidthError'])}>
-        <h3>Please use a larger device to use CombatCommand.</h3>
-        <p>If you are currently using your phone in landscape mode, please turn it to portrait mode.</p>
-      </div>
-    );
-  }
   return (
     <div className={styles.App}>
       <AppBar maxWidth={MAX_WIDTH} />
