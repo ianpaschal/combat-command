b009bebbbcd54f2b4610b24fb73aa9de63a6442b
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-09-08 11:36:51 +0200

    WIP

--- convex/_model/tournamentCompetitors/table.ts
+++ convex/_model/tournamentCompetitors/table.ts
@@ -1,10 +1,18 @@
+import { RankingFactor } from '@ianpaschal/combat-command-static-data/flamesOfWarV4';
 import { defineTable } from 'convex/server';
 import { v } from 'convex/values';
+
+import { getStaticEnumConvexValidator } from '../common/_helpers/getStaticEnumConvexValidator';
 
 export const editableFields = {
   tournamentId: v.id('tournaments'),
   teamName: v.optional(v.string()),
   captainUserId: v.optional(v.id('users')),
+  scoreAdjustments: v.optional(v.array(v.object({
+    amount: v.number(),
+    round: v.number(),
+    factor: getStaticEnumConvexValidator(RankingFactor),
+  }))),
 };
 
 /**
