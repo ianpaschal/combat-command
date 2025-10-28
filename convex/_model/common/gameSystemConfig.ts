afecb406507876c81f29563064ac42ec5ab7e3bc
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-10-28 17:33:36 +0100

    #250 Migration (Part C)

--- convex/_model/common/gameSystemConfig.ts
+++ convex/_model/common/gameSystemConfig.ts
@@ -1,8 +1,14 @@
 import { FlamesOfWarV4, TeamYankeeV2 } from '@ianpaschal/combat-command-game-systems';
 import { v } from 'convex/values';
 import { zodToConvex } from 'convex-helpers/server/zod';
+import z from 'zod';
 
 export const gameSystemConfig = v.union(
-  zodToConvex(FlamesOfWarV4.gameSystemConfig.schema),
-  zodToConvex(TeamYankeeV2.gameSystemConfig.schema),
+  // TODO - MIGRATION: REMOVE AFTER MIGRATING:
+  zodToConvex(FlamesOfWarV4.gameSystemConfig.schema.innerType().extend({
+    useExperimentalMissions: z.boolean().optional(),
+  })),
+  zodToConvex(TeamYankeeV2.gameSystemConfig.schema.innerType().extend({
+    useExperimentalMissions: z.boolean().optional(),
+  })),
 );
