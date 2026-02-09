7fd18767400f9df9bfad9214cd2f63a72b1dc5c3
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-02-09 21:53:58 +0100

    Update createMockTournament.ts

--- convex/_fixtures/createMockTournament.ts
+++ convex/_fixtures/createMockTournament.ts
@@ -2,6 +2,7 @@ import {
   CurrencyCode,
   GameSystem,
   getGameSystem,
+  tournamentPairingConfig,
 } from '@ianpaschal/combat-command-game-systems/common';
 
 import { Doc } from '../_generated/dataModel';
@@ -44,6 +45,7 @@ export const createMockTournament = (
       currency: CurrencyCode.EUR,
     },
     roundCount: 5,
+    pairingConfig: tournamentPairingConfig.defaultValues,
     gameSystemConfig: gameSystemConfig.defaultValues,
     startsAt: Date.now() + (DAY_LENGTH_MS * 3),
     endsAt: Date.now() + (DAY_LENGTH_MS * 5),
